65563367e9851680f2070e50c250a344e4997fc9
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-29 07:26:12 +0100

    Updates

---
+++ precache-manifest.272e9e83febe5811de2562b68c1d6b60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604c0036b352bf1782463d0abf475563",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9d25edce3f430cd01c",
+    "url": "/static/css/main.238b5184.chunk.css"
+  },
+  {
+    "revision": "d82c8b201c0c70994f30",
+    "url": "/static/js/2.1cb293e2.chunk.js"
+  },
+  {
+    "revision": "2e9d25edce3f430cd01c",
+    "url": "/static/js/main.f4fc964d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
